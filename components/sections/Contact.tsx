8d2ea13b78fc938bae8b9574a7c778f828fe2d0c
Mohak Rathod <mohak2409@gmail.com>
2026-05-10 21:02:00 -0700

    Removed actual endpoint

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -33,7 +33,7 @@ export default function Contact() {
   e.preventDefault()
   setStatus('sending')
   try {
-    const res = await fetch('https://formspree.io/f/mojryvbg', {
+    const res = await fetch('https://formspree.io/f/YOUR_FORM_ID', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form),
